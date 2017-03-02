c377b0a902d97b03288abe8f3af0bf87b8634912
huangxiaoxu <huangxiaoxu93@gmail.com>
2017-03-02 13:20:07 +0800

    spring mongo

--- spring-boot-demo/src/main/java/com/findgods/fly/api/UserController.java
+++ spring-boot-demo/src/main/java/com/findgods/fly/api/UserController.java
@@ -1,6 +1,9 @@
 package com.findgods.fly.api;
 
+import com.findgods.fly.repository.UserRepository;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
 
 /**
  * REVIEW @RestController  注解相当于 @Controller 和 @ResponseBody
@@ -11,5 +14,14 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class UserController {
+
+    @Resource
+    UserRepository repository;
+
+    public String getUserByUsername() {
+        repository.findByusername("111111");
+        return "";
+    }
+
 
 }
